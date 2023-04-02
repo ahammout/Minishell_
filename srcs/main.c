caedb6e2720ce0d62de804adb30d18a9cf8d4d4d
Zakariae Essadqui <zessadqu@e1r4p5.1337.ma>
2023-04-02 01:48:54 +0000

    sa

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: zessadqu <zessadqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 11:14:07 by ahammout          #+#    #+#             */
-/*   Updated: 2023/04/02 01:19:29 by zessadqu         ###   ########.fr       */
+/*   Updated: 2023/04/02 01:48:40 by zessadqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int exitS;
 
-void ee()
-{
-    system("leaks minishell");
-}
+// void ee()
+// {
+//     system("leaks minishell");
+// }
 int main(int ac, char **av, char **envp)
 {
     t_data  data;
@@ -26,7 +26,7 @@ int main(int ac, char **av, char **envp)
     char    path[PATH_MAX];
     int     her_file = 0;
 
-    atexit(ee);
+    //atexit(ee);
     (void)**av;
     if (ac == 1)
     {
